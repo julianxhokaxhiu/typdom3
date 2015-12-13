fb9c9588350e6dc439fdeddffb2baad579401713
Julian Xhokaxhiu <julianxhokaxhiu@users.noreply.github.com>
2015-12-13 17:08:36 +0100

    Update ext_localconf.php
    Default configuration is no more available

--- ext_localconf.php
+++ ext_localconf.php
@@ -7,5 +7,3 @@ if (!defined('TYPO3_MODE')) {
 require_once t3lib_extMgm::extPath($_EXTKEY) . 'vendor/autoload.php';
 // Disable PDFDOM autoload, since we're using composer
 define('DOMPDF_ENABLE_AUTOLOAD', false);
-// Load the default configuration
-require_once t3lib_extMgm::extPath($_EXTKEY) . 'vendor/dompdf/dompdf/dompdf_config.inc.php';
